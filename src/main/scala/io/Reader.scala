08386442c4a76dbdbc0019996df0085bda7d89c2
Philippe Vinchon <p.vinchon@gmail.com>
2018-07-23 00:15:37 +0100

    Random reader to generate test cases

--- src/main/scala/io/Reader.scala
+++ src/main/scala/io/Reader.scala
@@ -3,6 +3,8 @@ package com.github.plippe.suprnation.io
 import cats.implicits._
 import cats.Applicative
 import cats.effect.Sync
+import java.util.concurrent.atomic.AtomicInteger
+import scala.util.Random
 
 trait Reader[F[_]] {
   def readLine(): F[Option[String]]
@@ -26,6 +28,26 @@ class IteratorReader[F[_]](lines: Iterator[String])(implicit F: Applicative[F])
     lines.hasNext match {
       case false => none[String].pure[F]
       case true  => lines.next.some.pure[F]
+    }
+  }
+
+}
+
+class RandomTreeReader[F[_]](seed: Int, maxLine: Int)(
+    implicit F: Applicative[F])
+    extends Reader[F] {
+
+  val maxValue = 10
+
+  val rnd = new Random(seed)
+  val currentLine = new AtomicInteger(1)
+
+  def readLine(): F[Option[String]] = {
+    currentLine.get() match {
+      case line if line > maxLine => none[String].pure[F]
+      case line =>
+        currentLine.compareAndSet(line, line + 1)
+        1.to(line).map(_ => rnd.nextInt(maxValue)).mkString(" ").some.pure[F]
     }
   }
 
